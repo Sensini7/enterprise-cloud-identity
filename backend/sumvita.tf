b81e2be24358f23eb91fd719e8e3553406c4c490
Sensini7 <pelekengaih@gmail.com>
2025-04-18 15:14:52 +0100

    commit

--- backend/sumvita.tf
+++ backend/sumvita.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate"
     storage_account_name = "sensiniadtfstate713"
-    container_name      = "tfstate"
-    key                 = "test-conditional-access-policies.tfstate"
+    container_name       = "tfstate"
+    key                  = "test-conditional-access-policies.tfstate"
   }
 }
--- tfvars/sumvita.tfvars
+++ tfvars/sumvita.tfvars
@@ -11,4 +11,4 @@ primary_region                      = "eastus"
 customer_name                       = "EZD"
 #allowed_ip_cidr_list                = ["68.252.244.132", "108.48.5.37"]
 #deployment_enterprise_app_object_id = "16b48dd7-a8ac-4c1f-a45c-22a4638dd84e"
-environment_name                    = "mgmt"
+environment_name = "mgmt"
